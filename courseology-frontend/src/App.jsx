738d930af734090eb074f9fc24faa2c6644215f2
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-13 20:06:13 +0100

    initial front end commit

---
+++ courseology-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" name="input-box" id="input-box" className='input-box'/>
+    </div>
+  );
+}
+
+export default App;
